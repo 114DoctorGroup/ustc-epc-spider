02cc1a907ab01c35fc5feab32eb8377e47d9048e
Songchao Wang <songchaow@outlook.com>
2019-10-13 23:52:22 +0800

    Implement replacing.

--- epc_main.py
+++ epc_main.py
@@ -1,6 +1,9 @@
 import requests
 import re
 import json
+from os import system
+from bs4 import BeautifulSoup
+from datetime import datetime
 
 json_str = ''
 with open('config.json') as f:
@@ -9,7 +12,7 @@ js = json.loads(json_str)
 stuid = js['stuno']
 passwd = js['passwd']
 
-
+root_site = 'http://epc.ustc.edu.cn'
 main_site = 'http://epc.ustc.edu.cn/main.asp'
 
 situational_dlg_page = 'http://epc.ustc.edu.cn/m_practice.asp?second_id=2001'
@@ -56,14 +59,161 @@ else:
     print('Login failed.')
     exit()
 
-# First, check study hous
-s.cookies.set('querytype','all')
-res = s.get(record_page)
-status_raw = res.text
+course_form_patt = re.compile(r'(<form action="(m_practice.asp\?second_id.*?)".*?</form>)', re.DOTALL)
+form_tag_patt = re.compile('(<form action="(.*?)".*?</form>)', re.DOTALL)
+td_tag_patt = re.compile('<td.*?</td>',re.DOTALL)
+datetime_patt = re.compile(r'(\d+)/(\d+)/(\d+)<br>(\d+):(\d+)-')
+name_in_td_patt = re.compile(r'<td.*?<a href.*?>(.*?)</a></td>')
 
-all_hours = int(re.search(r'已预约的交流英语学时:(\d+)', status_raw).group(1))
-studied_hours  = int(re.search(r'已获得的交流英语学时:(\d+)', status_raw).group(1))
-planned_hours = all_hours - studied_hours # not greater than 4
+# First, check study hours
+def check_study_hours(s):
+    s.cookies.set('querytype','all')
+    res = s.get(record_page)
+    status_raw = res.text
+    all_hours = int(re.search(r'已预约的交流英语学时:(\d+)', status_raw).group(1))
+    studied_hours  = int(re.search(r'已获得的交流英语学时:(\d+)', status_raw).group(1))
+    planned_hours = all_hours - studied_hours # not greater than 4
+    available_hours = 4 - planned_hours
+
+    # Check the earliest course that has been planned
+    earliest_dt = None
+    earliest_params = None
+    earliest_name = None
+
+    form_list_raw = form_tag_patt.findall(status_raw)[1::] # remove the first form, which is not a course
+    for form in form_list_raw:
+        td_list = td_tag_patt.findall(form[0])
+        dt_match = datetime_patt.search(td_list[6])
+        dt = datetime(int(dt_match.group(1)),int(dt_match.group(2)),int(dt_match.group(3)),int(dt_match.group(4)),int(dt_match.group(5)))
+        planned = '预约中' in td_list[9]
+        if planned and (earliest_dt is None or dt<earliest_dt):
+            earliest_dt = dt
+            earliest_params = form[1]
+            earliest_name = name_in_td_patt.search(td_list[0]).group(1)
+    print('Course candidate to be replaced: '+earliest_name+' at '+str(earliest_dt))
+    return available_hours, earliest_dt, earliest_params, earliest_name
+
+available_hours, earliest_dt, earliest_params, earliest_name = check_study_hours(s)
+
+print('Situ(1)\tTopic(2)\tDebate(2)\tDrama(2)')
+
+old_state = [0,0,0,0]
+
+week_patt = re.compile(r'<td align="center">第(\d+)周</td>')
 
 
+def check_earliest_course(s:requests.Session, page_url:str):
+    # TODO: return course params and a datetime obj
+    week_patt = re.compile(r'<td align="center">第(\d+)周</td>')
+    page_raw = s.get(page_url+'&isall=some').text
+    earliest_week = int(week_patt.search(page_raw).group(1))
+    course_params = course_form_patt.search(page_raw).group(2)
+    course_form = course_form_patt.search(page_raw).group(1)
+    td_list = td_tag_patt.findall(course_form)
+    dt_match = datetime_patt.search(td_list[5])
+    dt = datetime(int(dt_match.group(1)),int(dt_match.group(2)),int(dt_match.group(3)),int(dt_match.group(4)),int(dt_match.group(5)))
+    return [earliest_week, dt, course_params]
+
+def order(course_params: str):
+    book_form = {'submit_type':'book_submit',
+                '截止日期':'end_date'}
+    course_path = root_site + '/' + course_params
+    res = s.post(course_path,book_form)
+    succeed= not '操作失败' in res.text
+    if(succeed):
+        # TODO: add support for 1 point courses
+        available_hours -= 2
+    return succeed
+
+def cancel(cancel_params: str):
+    cancel_form = {'submit_type':'book_cancel',
+                '截止日期':'end_date'}
+    course_path = root_site + '/' + cancel_params
+    res = s.post(course_path,cancel_form)
+    succeed= not '操作失败' in res.text
+    if(succeed):
+        # TODO: add support for 1 point courses
+        available_hours += 2
+    return succeed
+
+def smart_order(course_params: str):
+    if(available_hours == 1):
+        print('Now we don\'t consider 1 point course.')
+        return
+    if(available_hours>=2):
+        if(order(course_params)):
+            return True
+        else:
+            return False
+    else:
+        # we're NOT considering the score being ONE!
+        if(not cancel(earliest_params)):
+            return False
+        if(available_hours>=2):
+            if(order(course_params)):
+                return True
+            else:
+                # first roll back
+                print('Failed. Rolling back...')
+                if(not order(earliest_params)):
+                    print('Roll back failed!')
+                else:
+                    print('Roll back succeed.')
+                return False
+
+#--- test part
+# Only print for situational dialog.
+while True:
+    situational_res = check_earliest_course(s,situational_dlg_page+'&isall=some')
+    print(str(situational_res[0]), end='\t', flush=True)
+    for page in [topical_discus_page, debate_page, drama_page]:
+        res = check_earliest_course(s, page+'&isall=some')
+        print(str(res[0]), end='\t', flush=True)
+        if(res[1]<earliest_dt):
+            print('发现可替代课程：'+str(res[1])+' 已选课程：'+str(earliest_dt)+' '+earliest_name)
+            print('正在换课...')
+            if(smart_order(res[2])):
+                print('换课成功！')
+                exit(0)
+            else:
+                print('换课失败')
+                exit(0)
+    print('')
+            # if(not r[0] and r[1]=='Order Failed'):
+            #     print('换课失败，且已退课，正在尝试回滚')
+
+            # elif(not r[0] and r[1]=='Cancel Failed'):
+            #     print('换课失败，但未退课')
+            # elif(r[0]):
+            #     print('换课成功')
+
+#--------------
+
+while True:
+    situatioinal_str = s.get(situational_dlg_page+'&isall=some').text
+    situational_week = int(week_patt.search(situatioinal_str).group(1))
+    print(str(situational_week), end='\t', flush=True)
+    if(situational_week<old_state[0]):
+        #send email
+        pass
+    topical_str = s.get(topical_discus_page+'&isall=some').text
+    topical_week = int(week_patt.search(topical_str).group(1))
+    print(str(topical_week), end='\t\t', flush=True)
+    if(topical_week<old_state[1]):
+        #send email
+        pass
+    debate_str = s.get(debate_page+'&isall=some').text
+    debate_week = int(week_patt.search(debate_str).group(1))
+    print(str(debate_week), end='\t', flush=True)
+    if(debate_week<old_state[2]):
+        #send email
+        pass
+    drama_week = int(week_patt.search(s.get(drama_page+'&isall=some').text).group(1))
+    print(str(drama_week), flush=True)
+    if(drama_week<old_state[3]):
+        #send email
+        pass
+    old_state = [situational_week, topical_week, debate_week, drama_week]
 pass
+
+
