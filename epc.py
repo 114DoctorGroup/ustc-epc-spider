05d731e0c6b50995f0778c6cbeae5316aca1385d
Luo Yongping <ypluo18@qq.com>
2019-11-27 12:21:00 +0800

    add email

--- epc.py
+++ epc.py
@@ -1,11 +1,13 @@
 import requests
 import re
 import os
+import sys
 import json
 from os import system
 from datetime import datetime, timedelta
 import check
-
+import mail
+import logging
 
 # constant parameter of epc website
 root_site = 'http://epc.ustc.edu.cn'
@@ -35,254 +37,267 @@ default_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Appl
  
 # First, check study hours
 def check_study_hours(s):
-    s.cookies.set('querytype','all')
-    res = s.get(record_page)
-    status_raw = res.text
+	s.cookies.set('querytype','all')
+	res = s.get(record_page)
+	status_raw = res.text
 
-    all_hours = int(re.search(r'已预约的交流英语学时:(\d+)', status_raw).group(1))
-    studied_hours  = int(re.search(r'已获得的交流英语学时:(\d+)', status_raw).group(1))
-    absent_hours  = int(re.search(r'预约未上的交流英语学时：(\d+)', status_raw).group(1))
-    planned_hours = all_hours - studied_hours - absent_hours # not greater than 4
-    available_hours = 4 - planned_hours
-    #print(all_hours, studied_hours, absent_hours)
+	all_hours = int(re.search(r'已预约的交流英语学时:(\d+)', status_raw).group(1))
+	studied_hours  = int(re.search(r'已获得的交流英语学时:(\d+)', status_raw).group(1))
+	absent_hours  = int(re.search(r'预约未上的交流英语学时：(\d+)', status_raw).group(1))
+	planned_hours = all_hours - studied_hours - absent_hours # not greater than 4
+	available_hours = 4 - planned_hours
+	#print(all_hours, studied_hours, absent_hours)
 
-    # Check the earliest course that has been planned
-    candidate_dt = datetime.now() + timedelta(days = 365) # 默认时间为明年同一天，标识未选上任何课程
-    candidate_params = None
-    candidate_name = None
+	# Check the earliest course that has been planned
+	candidate_dt = datetime.now() + timedelta(days = 365) # 默认时间为明年同一天，标识未选上任何课程
+	candidate_params = None
+	candidate_name = None
 
-    form_list_raw = form_tag_patt.findall(status_raw)[1::] # remove the first form, which is not a course
-    for form in form_list_raw:
-        td_list = td_tag_patt.findall(form[0])
-        dt_match = datetime_patt.search(td_list[6])
-        dt = datetime(int(dt_match.group(1)),int(dt_match.group(2)),int(dt_match.group(3)),int(dt_match.group(4)),int(dt_match.group(5)))
-        planned = '预约中' in td_list[9]
-        if len(replace_scandidate) > 0:
-            nm = name_in_td_patt.search(td_list[0]).group(1)
-            if(planned and replace_scandidate in nm):
-                candidate_dt, candidate_params, candidate_name = dt, form[1], nm
-            else:
-                continue
-        elif planned :
-            if candidate_params is None: # 当前还没有替换对象
-                candidate_dt = dt
-                candidate_params = form[1]
-                candidate_name = name_in_td_patt.search(td_list[0]).group(1)
-            elif dt > candidate_dt: # 用时间最晚的来作为替换对象
-                candidate_dt = dt
-                candidate_params = form[1]
-                candidate_name = name_in_td_patt.search(td_list[0]).group(1)
-            else:
-                pass
-            
-    return available_hours, candidate_dt, candidate_params, candidate_name
+	form_list_raw = form_tag_patt.findall(status_raw)[1::] # remove the first form, which is not a course
+	for form in form_list_raw:
+		td_list = td_tag_patt.findall(form[0])
+		dt_match = datetime_patt.search(td_list[6])
+		dt = datetime(int(dt_match.group(1)),int(dt_match.group(2)),int(dt_match.group(3)),int(dt_match.group(4)),int(dt_match.group(5)))
+		planned = '预约中' in td_list[9]
+		if len(replace_scandidate) > 0:
+			nm = name_in_td_patt.search(td_list[0]).group(1)
+			if(planned and replace_scandidate in nm):
+				candidate_dt, candidate_params, candidate_name = dt, form[1], nm
+			else:
+				continue
+		elif planned :
+			if candidate_params is None: # 当前还没有替换对象
+				candidate_dt = dt
+				candidate_params = form[1]
+				candidate_name = name_in_td_patt.search(td_list[0]).group(1)
+			elif dt > candidate_dt: # 用时间最晚的来作为替换对象
+				candidate_dt = dt
+				candidate_params = form[1]
+				candidate_name = name_in_td_patt.search(td_list[0]).group(1)
+			else:
+				pass
+			
+	return available_hours, candidate_dt, candidate_params, candidate_name
 
 def check_earliest_course(s:requests.Session, page_url:str):
-    # TODO: return course params and a datetime obj
-    week_patt = re.compile(r'<td align="center">第(\d+)周</td>')
-    page_raw = s.get(page_url+'&isall=some').text
-    earliest_week = int(week_patt.search(page_raw).group(1))
-    course_params = course_form_patt.search(page_raw).group(2)
-    course_form = course_form_patt.search(page_raw).group(1)
-    td_list = td_tag_patt.findall(course_form)
+	# TODO: return course params and a datetime obj
+	week_patt = re.compile(r'<td align="center">第(\d+)周</td>')
+	page_raw = s.get(page_url+'&isall=some').text
+	earliest_week = int(week_patt.search(page_raw).group(1))
+	course_params = course_form_patt.search(page_raw).group(2)
+	course_form = course_form_patt.search(page_raw).group(1)
+	td_list = td_tag_patt.findall(course_form)
 
-    dt_match = datetime_patt.search(td_list[5])
-    dt = datetime(int(dt_match.group(1)),int(dt_match.group(2)),int(dt_match.group(3)),int(dt_match.group(4)),int(dt_match.group(5)))
+	dt_match = datetime_patt.search(td_list[5])
+	dt = datetime(int(dt_match.group(1)),int(dt_match.group(2)),int(dt_match.group(3)),int(dt_match.group(4)),int(dt_match.group(5)))
 
-    weekday_patt = re.compile(r'<td align="center">周(\S)</td>')
-    weekday = weekday_patt.search(td_list[2]).group(1)
+	weekday_patt = re.compile(r'<td align="center">周(\S)</td>')
+	weekday = weekday_patt.search(td_list[2]).group(1)
 
-    return [earliest_week, dt, course_params, weekday]
+	return [earliest_week, dt, course_params, weekday]
 
 def find_alternative(s:requests.Session, page_url:str, type_code:int):
-    page_raw = s.get(page_url+'&isall=some').text
+	page_raw = s.get(page_url+'&isall=some').text
 
-    all_course_form = course_form_patt2.findall(page_raw)
-    
-    if all_course_form is None:
-        exit(-1)
-        
-    for course_form in all_course_form:
-        course_params = course_form_patt.search(course_form).group(2)
-        td_list = td_tag_patt.findall(course_form)
-    
-        dt_match = datetime_patt.search(td_list[5])
-        dt = datetime(int(dt_match.group(1)),int(dt_match.group(2)),int(dt_match.group(3)),int(dt_match.group(4)),int(dt_match.group(5)))
-    
-        weekday = weekday_patt.search(td_list[2]).group(1)
+	all_course_form = course_form_patt2.findall(page_raw)
+	
+	if len(all_course_form) == 0:
+		exit(-1)
+		
+	for course_form in all_course_form:
+		course_params = course_form_patt.search(course_form).group(2)
+		td_list = td_tag_patt.findall(course_form)
+	
+		dt_match = datetime_patt.search(td_list[5])
+		dt = datetime(int(dt_match.group(1)),int(dt_match.group(2)),int(dt_match.group(3)),int(dt_match.group(4)),int(dt_match.group(5)))
+	
+		weekday = weekday_patt.search(td_list[2]).group(1)
 
-        course = [dt, course_params, weekday]
+		course = [dt, course_params, weekday]
 
-        if "预约时间未到" in course_form: # 预约时间未到
-            break
-        elif "您已经预约过该时间段的课程" in course_form \
-            or "已选择过该教师与话题相同的课程，不能重复选择" in course_form \
-            or "取 消" in course_form:
-            pass
-        elif is_wanted_dt(course, class_type[type_code]): # find the first(earliest) course in this type
-            return course
-        else:
-            pass
+		if "预约时间未到" in course_form: # 预约时间未到
+			break
+		elif "您已经预约过该时间段的课程" in course_form \
+			or "已选择过该教师与话题相同的课程，不能重复选择" in course_form \
+			or "取 消" in course_form:
+			pass
+		elif is_wanted_dt(course, class_type[type_code]): # find the first(earliest) course in this type
+			print("找到最早合适课程:" + class_type[type_code] + " " + str(course[0]))
+			#logging.info("找到最早合适课程:" + class_type[type_code] + " " + str(course[0]))
+			return course
+		else:
+			pass
 
-    return None
+	return None
 
-def order(course_params: str):
-    book_form = {'submit_type':'book_submit',
-                '截止日期':'end_date'}
-    course_path = root_site + '/' + course_params
-    res = s.post(course_path,book_form)
-    succeed= not '操作失败' in res.text
-    return succeed
+def order(course_params: str, course_time_str: str):
+	book_form = {'submit_type':'book_submit',
+				'截止日期':'end_date'}
+	course_path = root_site + '/' + course_params
+	res = s.post(course_path, book_form)
+	succeed= not '操作失败' in res.text
+	if succeed:
+		mail.SendMail("你已选上新课程: " + course_time_str, sender, mail_passwd)
+	return succeed
 
 def cancel(cancel_params: str):
-    cancel_form = {'submit_type':'book_cancel',
-                '截止日期':'end_date'}
-    course_path = root_site + '/' + cancel_params
-    res = s.post(course_path,cancel_form)
-    succeed= not '操作失败' in res.text
-    global available_hours
-    if(succeed):
-        # TODO: add support for 1 point courses
-        available_hours += 2
-    return succeed
+	cancel_form = {'submit_type':'book_cancel',
+				'截止日期':'end_date'}
+	course_path = root_site + '/' + cancel_params
+	res = s.post(course_path,cancel_form)
+	succeed= not '操作失败' in res.text
+	global available_hours
+	if(succeed):
+		# TODO: add support for 1 point courses
+		available_hours += 2
+	return succeed
 
-def smart_order(course_params: str):
-    global available_hours, candidate_dt, candidate_params, candidate_name
+def smart_order(course_params: str, course_time_str: str):
+	global available_hours, candidate_dt, candidate_params, candidate_name
 
-    if(available_hours == 1):
-        print('Now we don\'t consider 1 point course.')
-        return
-    if(available_hours >= 2):
-        print('可用预约学时足够，直接选课')
-        if(order(course_params)):
-            return True
-        else:
-            return False
-    elif(replace_flag):
-        # we're NOT considering the score being ONE!
-        print('正在换课，将退课程：'+ str(candidate_dt)+' '+candidate_name)
-        if(not cancel(candidate_params)):
-            return False
-        if(available_hours>=2):
-            print('正在选课...')
-            if(order(course_params)):
-                return True
-            else:
-                # first roll back
-                print('Failed. Rolling back...')
-                candidate_params_order = candidate_params.replace('record_book.asp','m_practice.asp')
-                if(not order(candidate_params_order)):
-                    print('Roll back failed!')
-                else:
-                    print('Roll back succeed.')
-                return False
-    else:
-        print('可用预约学时不足')
+	if(available_hours == 1):
+		print('Now we don\'t consider 1 point course.')
+		return
+	if(available_hours >= 2):
+		print('可用预约学时足够，直接选课')
+		logging.info('可用预约学时足够，直接选课')
+		if(order(course_params, course_time_str)):
+			return True
+		else:
+			return False
+	elif(replace_flag):
+		# we're NOT considering the score being ONE!
+		print('正在换课，将退课程：'+ str(candidate_dt)+' '+candidate_name)
+		logging.info('正在换课，将退课程：'+ str(candidate_dt)+' '+ candidate_name)
+		if(not cancel(candidate_params)):
+			return False
+		if(available_hours>=2):
+			if(order(course_params, course_time_str)):
+				return True
+			else:
+				return False
+	else:
+		print('可用预约学时不足')
 
 def is_wanted_dt(course: tuple, type) :
-    course_date_str = '周' + course[2]
-    
-    if course[0].hour == 8 or course[0].hour == 9:
-        course_date_str += '上午'
-    if course[0].hour == 14:
-        course_date_str += '下午'
-    if course[0].hour == 19:
-        course_date_str += '晚上'
+	course_date_str = '周' + course[2]
+	
+	if course[0].hour >= 8 and course[0].hour < 12:
+		course_date_str += '上午'
+	elif course[0].hour >= 14 and course[0].hour < 17:
+		course_date_str += '下午'
+	elif course[0].hour >= 19:
+		course_date_str += '晚上'
 
-    if course_date_str in banned_time :
-        print("时间需求不满足：" + type + " " + str(course[0]) + "(" + course_date_str + ")")
-        return False
-    else :
-        return True
+	if course_date_str in banned_time :
+		print("时间需求不满足:" + type + " " + str(course[0]) + "(" + course_date_str + ")")
+		return False
+	else :
+		return True
 
 def OrderCourseLoop(s):
-    print('Situ(1)\tTopi(2)\tDeba(2)\tDrama(2)')
-    
-    global available_hours, candidate_dt, candidate_params, candidate_name
-    
-    available_hours, candidate_dt, candidate_params, candidate_name = check_study_hours(s)
+	print('Situ(1)\tTopic(2)\tDebate(2)\tDrama(2)')
+	
+	global available_hours, candidate_dt, candidate_params, candidate_name
+	
+	available_hours, candidate_dt, candidate_params, candidate_name = check_study_hours(s)
 
-    while True:
-        for i, page in enumerate([situational_dlg_page, drama_page, topical_discus_page, debate_page]):
-            if(not enable_array[i]):
-                continue
-                
-            res = find_alternative(s, page+'&isall=some', i)
+	logging.info("Candidate: " + candidate_name + " " + str(candidate_dt))
+	while True:
+		for i, page in enumerate([situational_dlg_page, drama_page, topical_discus_page, debate_page]):
+			if(not enable_array[i]):
+				continue
+			
+			res = find_alternative(s, page+'&isall=some', i)
 
-            if res is not None:
-                if available_hours > 0:
-                    smart_order(res[1])
-                    available_hours, candidate_dt, candidate_params, candidate_name = check_study_hours(s)
-                elif res[0].day < candidate_dt.day:
-                    print('发现日期更早的可替代课程：' + class_type[i] + " " + str(res[0]))
-                    smart_order(res[1])
-                    available_hours, candidate_dt, candidate_params, candidate_name = check_study_hours(s)
-                elif res[0].day == candidate_dt.day:
-                    print('发现同一天的替代课程：' + class_type[i] + " " + str(res[0]))
-                else:
-                    pass
-            
+			if res is not None:
+				if available_hours > 0:
+					smart_order(res[1], class_type[i] + " " + str(res[0]))
+					#available_hours, candidate_dt, candidate_params, candidate_name = check_study_hours(s)
+				elif (candidate_dt - res[0]).days >= 1:
+					print('发现日期更早的可替代课程：' + class_type[i] + " " + str(res[0]))
+					logging.info('发现日期更早的可替代课程：' + class_type[i] + " " + str(res[0]))
+					smart_order(res[1], class_type[i] + " " + str(res[0]))
+					#available_hours, candidate_dt, candidate_params, candidate_name = check_study_hours(s)
+					#logging.info("Candidate Now: " + candidate_name + " " + str(candidate_dt))
+				elif res[0].month == candidate_dt.month and res[0].day == candidate_dt.day:
+					print('发现同一天的替代课程：' + class_type[i] + " " + str(res[0]))
+				else:
+					pass
+		# each iteration update the candidate
+		available_hours, candidate_dt, candidate_params, candidate_name = check_study_hours(s)
+			
 if __name__ == "__main__":
-    # read the configuration in the json file
-    json_str = ''
-    with open('config.json', encoding = 'utf-8') as f:
-        json_str = f.read()
-    js = json.loads(json_str)
-    stuid = js['stuno']
-    passwd = js['passwd']
-    order_flag = js['enable.order']
-    replace_flag = js['enable.replace']
-    replace_scandidate = js['replace_candidate']
-    banned_time = js['banned_time']
-    enable_array = [js['enable.situational_dialog'], js['enable.drama'], js['enable.topical_discuss'], js['enable.debate']]
-    
-    # candidate course to replace, global parameters
-    available_hours = 0
-    candidate_dt = None
-    candidate_params = None
-    candidate_name = ''
-    
-    while True:     
-        try:
-            s = requests.Session()
-            s.headers.update(default_headers)
-            s.get(main_site)
+	# read the configuration in the json file
+	json_str = ''
+	
+	if len(sys.argv) > 1:
+		config_file = sys.argv[1]
+	else:
+		config_file = 'config.json'
+	
+	logging.basicConfig(filename = config_file + '.log', level = logging.INFO)
+	with open(config_file, encoding = 'utf-8') as f:
+		json_str = f.read()
+	js = json.loads(json_str)
+	stuid = js['stuno']
+	passwd = js['passwd']
+	order_flag = js['enable.order']
+	replace_flag = js['enable.replace']
+	replace_scandidate = js['replace_candidate']
+	banned_time = js['banned_time']
+	enable_array = [js['enable.situational_dialog'], js['enable.drama'], js['enable.topical_discuss'], js['enable.debate']]
+	
+	sender = js["ustc_mail"]
+	mail_passwd = js["mail_passwd"]
 
-            # get check code's timestamp
-            nleft_text = s.get(nleft_page).text
-            timestamp = None
-            try:
-                timestamp = re.search('<TD ><img src="(.*?)">', nleft_text).group(1)
-            except Exception:
-                print("Parse error.")
-            timestamp = timestamp.split('checkcode.asp?')[1]
-            img = s.get(checkcode_path, params = {timestamp:None}).content
-            with open('checkcode.png','wb') as f:
-                f.write(img)
+	# candidate course to replace, global parameters
+	available_hours = 0
+	candidate_dt = None
+	candidate_params = None
+	candidate_name = ''
+	
+	while True:	 
+		try:
+			s = requests.Session()
+			s.headers.update(default_headers)
+			s.get(main_site)
 
-            checkcode = check.Checkcode()
-            print("checkcode: " + checkcode)
-            login_dict = {'submit_type': 'user_login',
-                'name': stuid,
-                'pass': passwd,
-                'txt_check': checkcode,
-                'user_type': 2,
-                'Submit': 'LOG IN'
-                }
-            res = s.post(nleft_page, data=login_dict)
-            s.get(main_site)
-            nleft_text = s.get(nleft_page).text
+			# get check code's timestamp
+			nleft_text = s.get(nleft_page).text
+			timestamp = None
+			try:
+				timestamp = re.search('<TD ><img src="(.*?)">', nleft_text).group(1)
+			except Exception:
+				print("Parse error.")
+			timestamp = timestamp.split('checkcode.asp?')[1]
+			img = s.get(checkcode_path, params = {timestamp:None}).content
+			with open('checkcode.png','wb') as f:
+				f.write(img)
 
-            if res.status_code == 200 and "点击可注销本次登录" in nleft_text:
-                print('Logined.')
-                OrderCourseLoop(s)
-            else:
-                print('Login failed. Check your password')
-                os._exit(-1)
+			checkcode = check.Checkcode()
+			print("checkcode: " + checkcode)
+			login_dict = {'submit_type': 'user_login',
+				'name': stuid,
+				'pass': passwd,
+				'txt_check': checkcode,
+				'user_type': 2,
+				'Submit': 'LOG IN'
+				}
+			res = s.post(nleft_page, data=login_dict)
+			s.get(main_site)
+			nleft_text = s.get(nleft_page).text
 
-        except KeyboardInterrupt:
-            print("Stop the process with a keyboard interupt!")
-            os._exit(-1)
+			if res.status_code == 200 and "点击可注销本次登录" in nleft_text:
+				print('Logined.')
+				OrderCourseLoop(s)
+			else:
+				print('Login failed. Check your password')
+				os._exit(-1)
 
-        except:
-            print("something bad happened! connect again")
+		except KeyboardInterrupt:
+			print("Stop the process with a keyboard interupt!")
+			os._exit(-1)
+
+		except:
+			print("something bad happened! connect again")
---
+++ mail.py
@@ -0,0 +1,19 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+def SendMail(mail_content, sender, passwd) :
+	try:
+		message = MIMEText(mail_content, 'plain', 'utf-8')
+		message['From'] = Header("EPC选课通知", 'utf-8')   # 发送者
+		message['To'] =  Header("EPC选课通知", 'utf-8')		# 接收者
+		message['Subject'] = Header('EPC', 'utf-8')
+		
+		mail_host = "mail.ustc.edu.cn"
+		server = smtplib.SMTP(mail_host, 25)
+		server.login(sender, passwd)
+
+		server.sendmail(sender, [sender], message.as_string())
+		server.quit()
+	except smtplib.SMTPException:
+		print("Send Email Failed")
